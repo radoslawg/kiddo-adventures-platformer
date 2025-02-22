ba55bdc981dc0dc23802baad9db3747289592374
Radosław Grzanka <radoslawg@gmail.com>
2025-02-22 12:40:28 +0100

    Audio Player

---
+++ Scenes/System/AudioPlayer.cs
@@ -0,0 +1,40 @@
+//-----------------------------------------------------------------------
+// <copyright file="AudioPlayer.cs" company="Radosław Grzanka">
+//     Copyright (c) Radosław Grzanka. Licensed under the GPL-3.0 license.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace org.grzanka.Kiddo.Audio;
+
+using System.Collections.Generic;
+using System.Linq;
+using Godot;
+
+public partial class AudioPlayer : Node
+{
+    public static AudioStream Coin => ResourceLoader.Load("res://assets/audio/coin.wav") as AudioStream;
+
+    private List<AudioStreamPlayer2D> AudioPlayers { get; set; }
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        AudioPlayers = GetNode<Node>("AudioPlayers")
+            .GetChildren()
+            .Cast<AudioStreamPlayer2D>()
+            .ToList();
+    }
+
+    public void PlaySound(AudioStream sound)
+    {
+        foreach (var audioStreamPlayer in AudioPlayers)
+        {
+            if (!audioStreamPlayer.IsPlaying())
+            {
+                audioStreamPlayer.Stream = sound;
+                audioStreamPlayer.Play();
+                return;
+            }
+        }
+    }
+}
